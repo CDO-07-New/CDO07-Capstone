ead69b27962c0343a6c7748232784c8820dba20d
ArtinaxW <161110817+BuiThanhNghiaDTU19122004@users.noreply.github.com>
2026-07-01 10:32:10 +0700

    fix: sns threshold

--- xbrain-learners/capstone/tf-4/cdo-07/infra/modules/cost-circuit-breaker/main.tf
+++ xbrain-learners/capstone/tf-4/cdo-07/infra/modules/cost-circuit-breaker/main.tf
@@ -336,13 +336,15 @@ resource "aws_budgets_budget" "monthly_cost" {
   time_unit    = "MONTHLY"
 
   dynamic "notification" {
-    for_each = compact([
-      aws_sns_topic.budget_warning.arn,
-      var.alert_sns_topic_arn
-    ])
+    for_each = {
+      for idx, arn in compact([
+        aws_sns_topic.budget_warning.arn,
+        var.alert_sns_topic_arn
+      ]) : idx => arn
+    }
     content {
       comparison_operator       = "GREATER_THAN"
-      threshold                 = var.warning_threshold_percent
+      threshold                 = var.warning_threshold_percent + (notification.key * 0.01)
       threshold_type            = "PERCENTAGE"
       notification_type         = "ACTUAL"
       subscriber_sns_topic_arns = [notification.value]
@@ -350,13 +352,15 @@ resource "aws_budgets_budget" "monthly_cost" {
   }
 
   dynamic "notification" {
-    for_each = compact([
-      aws_sns_topic.budget_hard_trigger.arn,
-      var.alert_sns_topic_arn
-    ])
+    for_each = {
+      for idx, arn in compact([
+        aws_sns_topic.budget_hard_trigger.arn,
+        var.alert_sns_topic_arn
+      ]) : idx => arn
+    }
     content {
       comparison_operator       = "GREATER_THAN"
-      threshold                 = var.hard_threshold_percent
+      threshold                 = var.hard_threshold_percent + (notification.key * 0.01)
       threshold_type            = "PERCENTAGE"
       notification_type         = "ACTUAL"
       subscriber_sns_topic_arns = [notification.value]
